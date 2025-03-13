9f51e0a3fd13118148a8704fd034329c80941532
FatDangg <115024656+FatDangg@users.noreply.github.com>
2025-03-13 15:03:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById('visitor-form').addEventListener('submit', async functio
 
     try {
         // Send data to backend
-        const response = await fetch('http://127.0.0.1:5000/submit', {
+        const response = await fetch('https://kas-visitor-system-i5hj.onrender.com/submit', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
